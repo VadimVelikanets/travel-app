ad5252a13a53f02e50aeec82def97c6c8e68385d
VadimVelikanets <52658557+VadimVelikanets@users.noreply.github.com>
2021-03-17 21:03:52 +0300

    Delete client directory

--- client/src/hooks/message.hook.js
+++
@@ -1,9 +0,0 @@
-import {useCallback} from 'react'
-//Хук на вывод сообщения
-export const useMessage = () => {
-    return useCallback(text => {
-        if (text) {
-            return  text
-        }
-    }, [])
-}
